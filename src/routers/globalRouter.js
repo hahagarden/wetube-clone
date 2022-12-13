e02c7851a8c9a8db72c84472d83cfbf0187ea3ac
hahagarden <tingyuan413@gmail.com>
2022-12-13 13:31:53 +0900

    save and bring real db

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { join } from "../controllers/userController";
-import { trending } from "../controllers/videoController";
+import { home } from "../controllers/videoController";
 import { login } from "../controllers/userController";
 import { search } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
-globalRouter.get("/", trending);
+globalRouter.get("/", home);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
 globalRouter.get("/search", search);
