2aa4675893bebf8f475a821d0417012f78cb9650
hahagarden <tingyuan413@gmail.com>
2022-12-16 22:40:30 +0900

    controller and template: login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => {
   return res.render("join", { pageTitle: "Join" });
@@ -22,6 +23,26 @@ export const postJoin = async (req, res) => {
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle: "Login",
+      errorMessage: "An account with this username does not exists.",
+    });
+  }
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle: "Login",
+      errorMessage: "Wrong password.",
+    });
+  }
+  console.log("login success! welcome.");
+  return res.redirect("/");
+};
 export const logout = (req, res) => res.send("Logout");
 export const see = (req, res) => res.send("Watch");
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { getJoin, join, postJoin } from "../controllers/userController";
 import { home } from "../controllers/videoController";
-import { login } from "../controllers/userController";
+import { getLogin, postLogin } from "../controllers/userController";
 import { search } from "../controllers/videoController";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
 rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.get("/login", login);
+rootRouter.route("/login").get(getLogin).post(postLogin);
 rootRouter.get("/search", search);
 
 export default rootRouter;
