96f6b479838fc696c9b34251d1a40f10b6380cf1
hahagarden <tingyuan413@gmail.com>
2023-01-21 20:55:28 +0900

    environment vars and dotenv

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://127.0.0.1:27017/wetube", {});
+mongoose.connect(process.env.DB_URL, {});
 
 const db = mongoose.connection; //mongoose가 우리 서버와 데이터베이스 서버를 연결시켜주는 역할을 함.
 
