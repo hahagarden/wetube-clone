3ab9cf5d0ea49ee43a791f50ae868a902bec3664
hahagarden <tingyuan413@gmail.com>
2023-01-25 16:51:39 +0900

    fetch github public user data using access token(not email yet)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -21,6 +21,48 @@ export const postJoin = async (req, res) => {
       .render("join", { pageTitle: "Join", errorMessage: error.message });
   }
 };
+
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const tokenRequest = await (
+    await fetch(finalUrl, {
+      method: "POST",
+      headers: { Accept: "application/json" },
+    })
+  ).json();
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const userData = await (
+      await fetch("https://api.github.com/user", {
+        method: "GET",
+        headers: { Authorization: `Bearer ${access_token}` },
+      })
+    ).json();
+    console.log(userData);
+  } else {
+    return res.redirect("/login");
+  }
+};
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const getLogin = (req, res) =>
