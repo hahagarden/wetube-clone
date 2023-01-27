eb745e7198adb54c6380bb53451032a053d22d01
hahagarden <tingyuan413@gmail.com>
2023-01-27 13:34:38 +0900

    protector and public Middlewares

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -3,12 +3,17 @@ import { getJoin, join, postJoin } from "../controllers/userController";
 import { home } from "../controllers/videoController";
 import { getLogin, postLogin } from "../controllers/userController";
 import { search } from "../controllers/videoController";
+import { publicOnlyMiddleware } from "../middlewares";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
-rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.route("/login").get(getLogin).post(postLogin);
+rootRouter.route("/join").all(publicOnlyMiddleware).get(getJoin).post(postJoin);
+rootRouter
+  .route("/login")
+  .all(publicOnlyMiddleware)
+  .get(getLogin)
+  .post(postLogin);
 rootRouter.get("/search", search);
 
 export default rootRouter;
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -7,13 +7,21 @@ import {
   postUpload,
   getUpload,
 } from "../controllers/videoController";
+import { protectorMiddleware } from "../middlewares";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", watch);
-videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
-videoRouter.get("/:id([0-9a-f]{24})/delete", deleteVideo);
-videoRouter.get("/upload", getUpload);
-videoRouter.post("/upload", postUpload);
+videoRouter
+  .route("/:id([0-9a-f]{24})/edit")
+  .all(protectorMiddleware)
+  .get(getEdit)
+  .post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})/delete", protectorMiddleware, deleteVideo);
+videoRouter
+  .route("/upload")
+  .all(protectorMiddleware)
+  .get(getUpload)
+  .post(postUpload);
 
 export default videoRouter;
