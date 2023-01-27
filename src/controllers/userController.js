58133d143d0152f43775e6b9e0879e49ed5c3866
hahagarden <tingyuan413@gmail.com>
2023-01-27 14:35:51 +0900

    fix join(add validation)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,7 +6,21 @@ export const getJoin = (req, res) => {
 };
 
 export const postJoin = async (req, res) => {
-  const { name, email, username, password, location } = req.body;
+  const { name, email, username, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  if (password !== password2) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "This email/username is already taken.",
+    });
+  }
   try {
     await User.create({
       name,
