a8e070311f1d7c23dd23e40b8d7cac6772826a14
hahagarden <tingyuan413@gmail.com>
2023-01-31 08:50:22 +0900

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/client/js/main.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "assets", "js"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
